3ce68e33a28c70c7de28ee12cb56174baef4e147
Rick Myers <slipdizzy@gmail.com>
2017-08-22 15:09:57 -0400

    Third commit from Eclipse through command line

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
 		System.out.println("Change added on GitHub");
+		System.out.println("Change added on Eclipse and pushed through command line");
 	}
 
 }
